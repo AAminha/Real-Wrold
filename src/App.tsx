b3bc2fb3d6524818e69b4385c23e8932649900bd
aaminha <dks4857@gmail.com>
2023-07-01 20:55:26 +0900

    💄 Header, Footer 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Route, Routes } from 'react-router-dom'
 
+import Footer from './components/Footer'
+import Header from './components/Header'
 import Article from './pages/Article'
 import Auth from './pages/Auth'
 import Create from './pages/Create'
@@ -11,40 +13,44 @@ import Settings from './pages/Settings'
 
 const App = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/login"
-        element={<Auth />}
-      />
-      <Route
-        path="/register"
-        element={<Auth />}
-      />
-      <Route
-        path="/editor"
-        element={<Create />}
-      />
-      <Route
-        path="/editor/:slug"
-        element={<Create />}
-      />
-      <Route
-        path="/settings"
-        element={<Settings />}
-      />
-      <Route
-        path="/:username"
-        element={<Profile />}
-      />
-      <Route
-        path="/article/:slug"
-        element={<Article />}
-      />
-    </Routes>
+    <main>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/login"
+          element={<Auth />}
+        />
+        <Route
+          path="/register"
+          element={<Auth />}
+        />
+        <Route
+          path="/editor"
+          element={<Create />}
+        />
+        <Route
+          path="/editor/:slug"
+          element={<Create />}
+        />
+        <Route
+          path="/settings"
+          element={<Settings />}
+        />
+        <Route
+          path="/:username"
+          element={<Profile />}
+        />
+        <Route
+          path="/article/:slug"
+          element={<Article />}
+        />
+      </Routes>
+      <Footer />
+    </main>
   )
 }
 
