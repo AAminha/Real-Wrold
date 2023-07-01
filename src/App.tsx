7cbb9c58e7dc62a75171abf11744b76a64af343b
aaminha <dks4857@gmail.com>
2023-07-01 18:18:50 +0900

    ✨ 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from 'react'
 
+import { Route, Routes } from 'react-router-dom'
+
+import Article from './pages/Article'
+import Auth from './pages/Auth'
+import Create from './pages/Create'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Settings from './pages/Settings'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/login"
+        element={<Auth />}
+      />
+      <Route
+        path="/register"
+        element={<Auth />}
+      />
+      <Route
+        path="/editor"
+        element={<Create />}
+      />
+      <Route
+        path="/editor/:slug"
+        element={<Create />}
+      />
+      <Route
+        path="/settings"
+        element={<Settings />}
+      />
+      <Route
+        path="/:username"
+        element={<Profile />}
+      />
+      <Route
+        path="/article/:slug"
+        element={<Article />}
+      />
+    </Routes>
+  )
 }
 
 export default App
