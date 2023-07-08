cffc8e6521e543487dccf72f32a05cc06d3254ad
aaminha <dks4857@gmail.com>
2023-07-08 23:11:28 +0900

    ✨ 로그인 여부에 따라 Navbar를 다르게 보여주도록 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,97 +1,16 @@
 import React from 'react'
 
-const Header = () => {
-  /* return (
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <a
-          className="navbar-brand"
-          href="index.html"
-        >
-          conduit
-        </a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <a
-              className="nav-link active"
-              href="/"
-            >
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href="/login"
-            >
-              Sign in
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href="/register"
-            >
-              Sign up
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  ) */
+import { useRecoilState } from 'recoil'
 
-  return (
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <a
-          className="navbar-brand"
-          href=""
-        >
-          conduit
-        </a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <a
-              className="nav-link active"
-              href=""
-            >
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href=""
-            >
-              {' '}
-              <i className="ion-compose"></i>&nbsp;New Article{' '}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href=""
-            >
-              {' '}
-              <i className="ion-gear-a"></i>&nbsp;Settings{' '}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href=""
-            >
-              <img
-                src=""
-                className="user-pic"
-              />
-              Eric Simons
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+import { userState } from '@/states/userState'
+
+import LoginNavbar from './LoginNavbar'
+import LogoutNavbar from './LogoutNavbar'
+
+const Header = () => {
+  const [currentUser] = useRecoilState(userState)
+
+  return currentUser ? <LoginNavbar /> : <LogoutNavbar />
 }
 
 export default Header
