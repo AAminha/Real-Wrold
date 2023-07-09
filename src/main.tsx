1e80e2f182733778392fbbed8fca002cb9c3c687
aaminha <dks4857@gmail.com>
2023-07-09 13:00:10 +0900

    ⚡️ react-query 옵션 설정

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,15 @@ import { RecoilRoot } from 'recoil'
 
 import App from './App'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+      // suspsnse: 아직 렌더링이 준비되지 않은 컴포넌트가 있을 때 로딩 화면을 보여주고 로딩이 완료되면 해당 컴포넌트를 보여주는 리액트 내장기능
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
