59c662543dba70737c7c02c37566ae1d05bdbfea
aaminha <dks4857@gmail.com>
2023-07-09 11:52:23 +0900

    ✨ 통신 제외 settings 구현

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { useNavigate } from 'react-router'
+import { useRecoilState } from 'recoil'
+
+import { userState } from '@/states/userState'
+import { PutUserInfoRequest, UserInfoType } from '@/types/auth'
 
 const Settings = () => {
+  const navigate = useNavigate()
+  const [userInfo, setUserInfo] = useRecoilState(userState)
+  const [newUserInfo, setNewUserInfo] = useState<PutUserInfoRequest>({
+    email: '',
+    password: '',
+    username: '',
+    bio: '',
+    image: '',
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(userInfo)
+    // 변경된 값 제출
+    navigate(`/profile/@${newUserInfo.username}`)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target
+    setNewUserInfo({ ...newUserInfo, [name]: value })
+  }
+
+  const handleLogout = () => {
+    setUserInfo(undefined)
+    navigate('/')
+  }
+
   return (
     <div className="settings-page">
       <div className="container page">
@@ -8,48 +41,68 @@ const Settings = () => {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Your Settings</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
                   <input
                     className="form-control"
                     type="text"
+                    name="image"
+                    value={newUserInfo.image}
                     placeholder="URL of profile picture"
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
                     type="text"
+                    name="username"
+                    value={newUserInfo.username}
                     placeholder="Your Name"
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <textarea
                     className="form-control form-control-lg"
                     rows={8}
+                    name="bio"
+                    value={newUserInfo.bio}
                     placeholder="Short bio about you"
+                    onChange={handleChange}
                   ></textarea>
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
                     type="text"
+                    name="email"
+                    value={newUserInfo.email}
                     placeholder="Email"
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
                     type="password"
+                    name="password"
+                    value={newUserInfo.password}
                     placeholder="Password"
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <button className="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
               </fieldset>
             </form>
             <hr />
-            <button className="btn btn-outline-danger">Or click here to logout.</button>
+            <button
+              className="btn btn-outline-danger"
+              onClick={handleLogout}
+            >
+              Or click here to logout.
+            </button>
           </div>
         </div>
       </div>
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,7 +1,18 @@
-export type UserInfoType = {
+export type GetUserInfoResponse = {
   email: string
   token: string
   username: string
   bio: string
   image: string
+}
+
+export type UserInfoType = {
+  email: string
+  username: string
+  bio: string
+  image: string
+}
+
+export type PutUserInfoRequest = UserInfoType & {
+  password: string
 }
