af026cfe51dc2559caabb8a723961112453d4a8f
aaminha <dks4857@gmail.com>
2023-07-21 13:44:46 +0900

    ✨ 로그인 페이지 구현 및 기본 기능 구

---
+++ src/pages/Login.tsx
@@ -0,0 +1,66 @@
+import React, { ChangeEvent, useState } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import { PostLoginRequest } from '@/types/auth'
+
+const Login = () => {
+  const [loginInfo, setLoginInfo] = useState<PostLoginRequest>({
+    email: '',
+    password: '',
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setLoginInfo({ ...loginInfo, [name]: value })
+  }
+
+  const handleSubmit = () => {
+    console.log('submit')
+  }
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link to="/register">Need an account?</Link>
+            </p>
+
+            <ul className="error-messages">
+              <li>That email is already taken</li>
+            </ul>
+
+            <form onSubmit={handleSubmit}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  name="email"
+                  value={loginInfo.email}
+                  placeholder="Email"
+                  onChange={handleInputChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  name="password"
+                  value={loginInfo.password}
+                  placeholder="Password"
+                  onChange={handleInputChange}
+                />
+              </fieldset>
+              <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
