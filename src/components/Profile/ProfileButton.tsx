a7443725d6167acb17ea4dd66ee6121f3c3c50fd
aaminha <dks4857@gmail.com>
2023-07-22 20:43:54 +0900

    ✨ 프로필 화면의 버튼 기능 구현

--- src/components/Profile/ProfileButton.tsx
+++ src/components/Profile/ProfileButton.tsx
@@ -1,28 +1,65 @@
 import React from 'react'
 
+import { useMutation } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 
+import { profileAPI } from '@/API/profile'
 import { userState } from '@/states/userState'
 
-const ProfileButton = ({ username, following }: { username?: string; following?: boolean }) => {
+const ProfileButton = ({
+  username,
+  following,
+  getProfileRefetch,
+}: {
+  username?: string
+  following?: boolean
+  getProfileRefetch: () => void
+}) => {
+  const navigate = useNavigate()
   const currentUser = useRecoilValue(userState)
+
+  const { mutate: postFollowMutate } = useMutation(profileAPI.follow, {
+    onSuccess: () => {
+      getProfileRefetch()
+    },
+    onError: () => {
+      // TODO : 에러 발생 알리기
+    },
+  })
+
+  const { mutate: deleteUnfollowMutate } = useMutation(profileAPI.unfollow, {
+    onSuccess: () => {
+      getProfileRefetch()
+    },
+    onError: () => {
+      // TODO : 에러 발생 알리기
+    },
+  })
+
+  const handleFollow = () => {
+    if (following) return postFollowMutate
+    return deleteUnfollowMutate
+  }
+
   if (currentUser?.username === username) {
     return (
-      <button className="btn btn-sm btn-outline-secondary action-btn">
+      <button
+        className="btn btn-sm btn-outline-secondary action-btn"
+        onClick={() => navigate('/settings')}
+      >
         <i className="ion-gear-a"></i>
         &nbsp; Edit Profile Settings
       </button>
     )
   } else {
-    return following ? (
-      <button className="btn btn-sm btn-outline-secondary action-btn">
+    return (
+      <button
+        className="btn btn-sm btn-outline-secondary action-btn"
+        onClick={handleFollow}
+      >
         <i className="ion-plus-round"></i>
-        &nbsp;&nbsp;{`Unfollow ${username}`}
-      </button>
-    ) : (
-      <button className="btn btn-sm btn-outline-secondary action-btn">
-        <i className="ion-plus-round"></i>
-        &nbsp;&nbsp;{`Follow ${username}`}
+        &nbsp;&nbsp;{`${following ? 'Unfollow' : 'Follow'} ${username}`}
       </button>
     )
   }
