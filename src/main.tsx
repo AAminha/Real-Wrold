e57757e2ccaad772cedb97862e36f0d5434a7b3d
aaminha <dks4857@gmail.com>
2023-07-22 23:10:00 +0900

    🔥 suspense 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,16 @@ import { RecoilRoot } from 'recoil'
 
 import App from './App'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      suspense: true,
-      // suspsnse: 아직 렌더링이 준비되지 않은 컴포넌트가 있을 때 로딩 화면을 보여주고 로딩이 완료되면 해당 컴포넌트를 보여주는 리액트 내장기능
-    },
-  },
-})
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback="..loading">
-        <RecoilRoot>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </RecoilRoot>
-      </Suspense>
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>
 )
