84a95ef8a13d0ca26187e3d23cc56e52dc1f29e2
aaminha <dks4857@gmail.com>
2023-08-13 21:29:37 +0900

    ⚡️ ArticleMeta 수정

--- src/components/Article/ArticleMeta.tsx
+++ src/components/Article/ArticleMeta.tsx
@@ -8,31 +8,24 @@ import { DateFormat } from '@/utils/DateFormat'
 import FavoriteButton from './FavoriteButton'
 import FollowButton from './FollowButton'
 
-const ArticleMeta = ({
-  article,
-  previewMode,
-}: {
-  article: ArticleData | undefined
-  previewMode: boolean
-}) => {
-  if (article)
-    return (
-      <div className="article-meta">
-        <Link to={`/@${article.author.username}`}>
-          <img src={article.author.image} />
+const ArticleMeta = ({ article, previewMode }: { article: ArticleData; previewMode: boolean }) => {
+  return (
+    <div className="article-meta">
+      <Link to={`/@${article.author.username}`}>
+        <img src={article.author.image} />
+      </Link>
+      <div className="info">
+        <Link
+          to={`/@${article.author.username}`}
+          className="author"
+        >
+          {article.author.username}
         </Link>
-        <div className="info">
-          <Link
-            to={`/@${article.author.username}`}
-            className="author"
-          >
-            {article.author.username}
-          </Link>
-          <span className="date">{DateFormat(article.createdAt)}</span>
-        </div>
-        {previewMode ? <FavoriteButton article={article} /> : <FollowButton />}
+        <span className="date">{DateFormat(article.createdAt)}</span>
       </div>
-    )
+      {previewMode ? <FavoriteButton article={article} /> : <FollowButton />}
+    </div>
+  )
 }
 
 export default ArticleMeta
--- src/components/Article/ArticlePreview.tsx
+++ src/components/Article/ArticlePreview.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 import { ArticleData } from '@/types/articles'
 
-// import ArticleMeta from './ArticleMeta'
+import ArticleMeta from './ArticleMeta'
 
 const ArticlePreview = ({
   articles,
@@ -23,35 +23,36 @@ const ArticlePreview = ({
   // TODO : 게시글이 없을 경우 'No articles are here... yet.'문구 보이도록
   return (
     <div>
-      {articles?.map((article: ArticleData) => (
-        <div
-          className="article-preview"
-          key={article.slug}
-        >
-          {/* <ArticleMeta
-            article={article}
-            previewMode={true}
-          /> */}
-          <Link
-            to={`/article/${article.slug}`}
-            className="preview-link"
+      {articles &&
+        articles.map((article: ArticleData) => (
+          <div
+            className="article-preview"
+            key={article.slug}
           >
-            <h1>{article.title}</h1>
-            <p>{article.description}</p>
-            <span>Read more...</span>
-            <ul className="tag-list">
-              {article.tagList.map((tag) => (
-                <li
-                  className="tag-default tag-pill tag-outline"
-                  key={tag}
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          </Link>
-        </div>
-      ))}
+            <ArticleMeta
+              article={article}
+              previewMode={true}
+            />
+            <Link
+              to={`/article/${article.slug}`}
+              className="preview-link"
+            >
+              <h1>{article.title}</h1>
+              <p>{article.description}</p>
+              <span>Read more...</span>
+              <ul className="tag-list">
+                {article.tagList.map((tag) => (
+                  <li
+                    className="tag-default tag-pill tag-outline"
+                    key={tag}
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            </Link>
+          </div>
+        ))}
     </div>
   )
 }
