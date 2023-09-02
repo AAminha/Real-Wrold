7770a2d596458153bea93a19152c9d6d8a31571b
aaminha <dks4857@gmail.com>
2023-09-02 13:39:51 +0900

    ✨ 비로그인 상태일 때 댓글 안 보이도록 구현

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
 
 import ArticleMeta from '@/components/Article/ArticleMeta'
 import Comment from '@/components/Comment'
 import { useGetArticle } from '@/hooks/useGetArticle'
+import { userState } from '@/states/userState'
 
 const Article = () => {
   const { slug } = useParams()
+  const [currentUser] = useRecoilState(userState)
   const { data: selectedArticle, refetch: getArticleRefetch } = useGetArticle(slug ? slug : '')
   return (
     <div className="article-page">
@@ -55,7 +58,16 @@ const Article = () => {
           )}
         </div>
 
-        <Comment />
+        {currentUser ? (
+          <Comment />
+        ) : (
+          <div className="col-xs-12 col-md-8 pffset-md-2">
+            <p style={{ display: 'inherit' }}>
+              <Link to="/login">Sign in</Link>&nbsp;or&nbsp;<Link to="/register">sign up</Link> to
+              add comments on this article.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   )
