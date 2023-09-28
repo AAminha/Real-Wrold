403f46ff1d879f17bf056fb247107e632ccd14e1
aaminha <dks4857@gmail.com>
2023-09-29 00:51:45 +0900

    ⚡️ 데이터 가져오기 실패 시 전부 홈으로 이동하도록 수정

--- src/hooks/useGetArticle.ts
+++ src/hooks/useGetArticle.ts
@@ -1,14 +1,18 @@
 import { useQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 
 import { articleAPI } from '@/API/articles'
 
 export const useGetArticle = (slug: string) => {
+  const navigate = useNavigate()
   return useQuery('getAritlce', () => articleAPI.getArticle(slug), {
     enabled: true,
+    retry: 0,
     onSuccess: (data) => {
       console.log(data)
     },
     onError: (err) => {
+      navigate('/')
       console.log(err)
     },
   })
--- src/hooks/useGetFollowArticles.ts
+++ src/hooks/useGetFollowArticles.ts
@@ -1,4 +1,5 @@
 import { useQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 
 import { articleAPI } from '@/API/articles'
 
@@ -13,15 +14,18 @@ export const useGetFollowArticles = ({
   favorited?: string
   page: number
 }) => {
+  const navigate = useNavigate()
   return useQuery(
     ['getFollowAritlces', tag, author, favorited, page],
     () => articleAPI.getFollowArticles({ page }),
     {
       enabled: false,
+      retry: 0,
       onSuccess: (data) => {
         console.log(data)
       },
       onError: (err) => {
+        navigate('/')
         console.log(err)
       },
     }
