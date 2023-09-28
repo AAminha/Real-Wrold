cffea8a8b0effc9193dca6d004c022f16293f43c
aaminha <dks4857@gmail.com>
2023-09-29 00:48:41 +0900

    ⚡️ 이상한 username의 경우 home으로 이동하도록 수정

--- src/hooks/useGetProfile.ts
+++ src/hooks/useGetProfile.ts
@@ -1,14 +1,18 @@
 import { useQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 
 import { profileAPI } from '@/API/profile'
 
 export const useGetProfile = (username: string) => {
+  const navigate = useNavigate()
   return useQuery(['getProfile', username], () => profileAPI.get(username), {
     enabled: true,
+    retry: 0,
     onSuccess: (data) => {
       console.log(data)
     },
     onError: (err) => {
+      navigate('/')
       console.log(err)
     },
   })
